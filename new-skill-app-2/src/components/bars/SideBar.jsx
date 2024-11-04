c95a089664ba4925291279587557a54b97aaef03
Claudia Pando <claudiamfp@hotmail.com>
2024-11-04 17:49:47 +0000

    add initial css to the homepage

--- new-skill-app-2/src/components/bars/SideBar.jsx
+++ new-skill-app-2/src/components/bars/SideBar.jsx
@@ -1,16 +1,22 @@
 import { NavLink } from "react-router-dom";
 
-function SideBar(){
+function SideBar() {
   return (
-    <aside>
-      <NavLink to="/">
-        <button className="">Home</button>
-      </NavLink>
 
-      <NavLink to="/AboutPage">
-        <button className="">About Page</button>
-      </NavLink>
-    </aside>
+    <div>
+
+      <aside className="nav flex-column nav-underline position-fixed vh-100"  style={{ width: "100px", top: "100px", left: "20px" }}>
+        
+        <NavLink to="/">
+          <a className="text-sm-center nav-link">Home</a>
+        </NavLink>
+
+        <NavLink to="/AboutPage">
+          <a className="text-sm-center nav-link">About Page</a>
+        </NavLink>
+      </aside>
+
+    </div>
   );
 };
 
--- new-skill-app-2/src/components/pages/HomePage.jsx
+++ new-skill-app-2/src/components/pages/HomePage.jsx
@@ -1,20 +1,25 @@
 import { NavLink } from "react-router-dom"
 
-function HomePage(){
-    return(
-        <div>
-            <h1>New Skill</h1>
-            <p>Pequena explicação catchy</p>
+function HomePage() {
+    return (
+        <div className="d-flex flex-column justify-content-center align-items-center vw-100">
 
-            <h2>Categories</h2>
-            <NavLink to="/VisualArtsPage">
-                <div className="card">Category 1 (Visual Arts)</div>
-            </NavLink>
-            
+            <div className="d-flex flex-column justify-content-center align-items-center vw-100">
+                <div className="text-center">
+                    <h1>New Skill</h1>
+                </div>
+
+                <div className="d-flex flex-column justify-content-center align-items-center vw-100">
+                    <p className="text-center">Pequena explicação catchy</p>
+                </div>
+            </div>
+
+
+
             <div className="card">Category 2</div>
             <div className="card">Category 3</div>
 
-        </div>
+        </div >
     )
 }
 
