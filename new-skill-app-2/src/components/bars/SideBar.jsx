444cee69432442d35d3e0585edbe7e9c8191bd7d
Mariana Frazão <mariana.franco.frazao@gmail.com>
2024-11-05 08:59:09 +0000

    corrected path to about page

--- new-skill-app-2/src/components/bars/SideBar.jsx
+++ new-skill-app-2/src/components/bars/SideBar.jsx
@@ -3,12 +3,6 @@ import { NavLink } from "react-router-dom";
 function SideBar() {
   return (
 
-<<<<<<< HEAD
-      <NavLink to="/about-page">
-        <button className="">About Page</button>
-      </NavLink>
-    </aside>
-=======
     <div>
 
       <aside className="nav flex-column nav-underline position-fixed vh-100"  style={{ width: "100px", top: "100px", left: "20px" }}>
@@ -17,13 +11,12 @@ function SideBar() {
           <a className="text-sm-center nav-link">Home</a>
         </NavLink>
 
-        <NavLink to="/AboutPage">
+        <NavLink to="/about-page">
           <a className="text-sm-center nav-link">About Page</a>
         </NavLink>
       </aside>
 
     </div>
->>>>>>> origin/main
   );
 };
 
