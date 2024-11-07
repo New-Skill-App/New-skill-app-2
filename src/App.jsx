93c88347c10b0989d09c40676f67315b89033607
Mariana Frazão <mariana.franco.frazao@gmail.com>
2024-11-07 15:45:44 +0000

    first try of responsiveness (needs improvement)

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import SideBar from "./components/bars/SideBar";
 import HomePage from "./components/pages/HomePage";
 import AboutPage from "./components/pages/AboutPage";
 import NotFoundPage from "./components/pages/NotFoundPage";
-// import VisualArtsPage from "./components/pages/VisualArtsPage";
 import SkillDetailsPage from "./components/pages/SkillDetailsPage";
 import CreateSkill from "./components/functionalities/CreateSkill";
 import SkillsList from "./components/pages/SkillsList";
--- src/components/bars/SideBar.jsx
+++ src/components/bars/SideBar.jsx
@@ -1,65 +1,78 @@
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import logoSideBar from "/images/logoSideBar.png";
 
+
 function SideBar() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
+    <>
+      <>
+        <button className="burger-menu" onClick={toggleMenu}>Burguer
+          <span className="burger-bar"></span>
+          <span className="burger-bar"></span>
+          <span className="burger-bar"></span>
+        </button>
 
-    <aside
-      className="sidebar nav flex-column nav-underline position-fixed shadow-lg">
+        <aside className={`sidebar nav flex-column nav-underline position-fixed shadow-lg ${isOpen ? 'open' : ''}`}>
+          <div className="sidebar-content d-flex flex-column">
+            <div className="sidebar-container">
+              <img src={logoSideBar} className="logo" alt="SkillBox Logo" />
+            </div>
 
-      <div className="sidebar-content d-flex flex-column">
+            <NavLink to="/">
+              <a className="nav-link-custom nav-link py-3">
+                <i class="bi bi-house-door"></i>
+                Home</a>
+            </NavLink>
 
-      <div className="sidebar-container">
-        <img src={logoSideBar} className="logo" alt="SkillBox Logo" />
-      </div>
+            <NavLink to="/about-page">
+              <a className="nav-link-custom nav-link py-3">
+                <i class="bi bi-file-earmark-person"></i>
+                About Us</a>
+            </NavLink>
 
-        <NavLink to="/">
-          <a className="nav-link-custom nav-link py-3">
-            <i class="bi bi-house-door"></i>
-            Home</a>
-        </NavLink>
+            <NavLink to="/skills/create">
+              <a className="nav-link-custom nav-link py-3">
+                <i class="bi bi-file-earmark-plus"></i>
+                Add Skill</a>
+            </NavLink>
 
-        <NavLink to="/about-page">
-          <a className="nav-link-custom nav-link py-3">
-            <i class="bi bi-file-earmark-person"></i>
-            About Us</a>
-        </NavLink>
+          </div>
+          <div className="sidebar-categories d-flex flex-column">
+            <NavLink to="/">
+              <a className="nav-link-custom nav-link py-3">
+                Categories</a>
+            </NavLink>
 
-        <NavLink to="/skills/create">
-          <a className="nav-link-custom nav-link py-3">
-            <i class="bi bi-file-earmark-plus"></i>
-            Add Skill</a>
-        </NavLink>
+            <NavLink to="/visual-arts-page">
+              <a className="nav-link-custom nav-link py-3">
+                <i class="bi bi-brush"></i>
+                Visual Arts</a>
+            </NavLink>
 
-      </div>
+            <NavLink to="/sports-page">
+              <a className="nav-link-custom nav-link py-3">
+                <i class="bi bi-bicycle"></i>
+                Sports</a>
+            </NavLink>
 
-      <div className="sidebar-categories d-flex flex-column">
-      <NavLink to="/">
-          <a className="nav-link-custom nav-link py-3">
-            Categories</a>
-        </NavLink>
+            <NavLink to="*">
+              <a className="nav-link-custom nav-link py-3">
+                <i class="bi bi-music-note-beamed"></i>
+                Music</a>
+            </NavLink>
+          </div>
+        </aside>
+      </>
 
-        <NavLink to="/visual-arts-page">
-          <a className="nav-link-custom nav-link py-3">
-          <i class="bi bi-brush"></i>
-            Visual Arts</a>
-        </NavLink>
-
-        <NavLink to="/sports-page">
-          <a className="nav-link-custom nav-link py-3">
-          <i class="bi bi-bicycle"></i>
-            Sports</a>
-        </NavLink>
-
-        <NavLink to="*">
-          <a className="nav-link-custom nav-link py-3">
-          <i class="bi bi-music-note-beamed"></i>
-          Music</a>
-        </NavLink>
-
-        </div>
-
-    </aside>
+    </>
   );
 };
 
